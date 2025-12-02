8bcb1e4a3b58c7def331099f671ea4afa6aaa2f1
ikromboy1 <ikromboykomiljanov761@gmail.com>
2025-12-02 20:59:13 +0500

    remote language error

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguagaDelector from "i18next-browser-languagedetector"
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import Uz from "./locales/Uz/translation.json"
-import Рус from "./locales/Рус/translation.json"
-import Узб from "./locales/Узб/translation.json"
+import Uz from "./locales/Uz/translation.json";
+import Рус from "./locales/Рус/translation.json";
+import Узб from "./locales/Узб/translation.json";
 
 i18n
-.use(LanguagaDelector) // Brauzerdan tilni avtomatik aniqlash
-.use(initReactI18next)
-.init({
+  .use(LanguageDetector) // avtomatik til aniqlash
+  .use(initReactI18next)
+  .init({
     resources: {
-        Uz: {translation: Uz},
-        Рус: {translation: Рус},
-        Узб: {translation: Узб},
+      Uz: { translation: Uz },
+      Рус: { translation: Рус },
+      Узб: { translation: Узб },
     },
-    failbacking: "Рус",
-    // interpolation: {
-    //     escapeValue: false,
-    // }
-})
+    fallbackLng: "Uz", // TO‘G‘RI Yozilishi shu
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
-export default i18n
+export default i18n;
